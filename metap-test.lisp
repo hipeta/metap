09618c048ddb86c2277c52233b264466f57d6020
hipeta <rhipeta@gmail.com>
2015-02-28 13:11:49 +0900

    Add compute-precedense-list test

--- metap-test.lisp
+++ metap-test.lisp
@@ -12,3 +12,19 @@
 
 (def-suite all)
 (in-suite all)
+
+(defclass a () ())
+(defclass b (a) ())
+(defclass c (a) ())
+(defclass d (b c) ())
+(defclass e (a) ())
+(defclass f (d e) ())
+(defclass g (d) ())
+(defclass h (g e) ())
+(defclass i (f) ())
+(defclass j (f) ())
+(defclass k (h i j) ())
+
+(test compute-precedense-list-test
+  (is (equal (metap::compute-precedense-list (mapcar #'find-class '(h i j)))
+             (cdr (c2mop:compute-class-precedence-list (find-class 'k))))))
